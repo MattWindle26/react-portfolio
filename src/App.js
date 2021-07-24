e1fc241065de00dcda69589245886656d5bc5241
Matt Windle <matthewwindle@outlook.com>
2021-07-24 21:03:35 +0100

    First commit of project to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import './Nav.css';
+import './Main.css';
 
 function App() {
+
+  const [Background, SetBackground] = useState("https://images.unsplash.com/photo-1499951360447-b19be8fe80f5")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container grid main-grid">     
+    <aside>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/b/b9/Slack_Technologies_Logo.svg" alt="" />
+        <h6>menu</h6>
+        <nav>
+          <ul>
+            <li><a href="" className="active"><span>Home</span></a></li>
+            <li><a href=""><span>About</span></a></li>
+            <li><a href="" ><span>Projects</span></a></li>
+            <li><a href=""><span>Contact</span></a></li>
+          </ul>
+        </nav>
+        <h6>projects</h6>
+        <nav>
+          <ul>
+            <li><a href=""><span>React blog</span></a></li>
+            <li><a href=""><span>React blog</span></a></li>
+            <li><a href="" ><span>React blog</span></a></li>
+            <li><a href=""><span>React blog</span></a></li>
+          </ul>
+        </nav>
+    </aside>
+    <main>
+        <h3>Most recent project</h3>
+        <div className="most-recent-project" style={{ backgroundImage: "url(" + Background + ")" }}>
+          <div className="mrp-overlay">
+              <h2>React blog</h2>
+              <p>A simple blog made from React for the front end and Node.js to handle the backend requests.</p>
+              <div className="mrp-btns">
+                <a href="" className="btn blue">Live project</a>
+              <a href="" className="btn border-white">Views docs</a>
+              </div>
+          </div>
+        </div>
+    </main>
     </div>
   );
 }
