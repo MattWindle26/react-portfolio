2bd696eb6cecb04be5d7fce00fe604ec7160e2ad
Matt Windle <matthewwindle@outlook.com>
2021-07-24 23:47:55 +0100

    Dynamic page titles
    Added dynamic page titles function

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './Nav.css';
 import './Main.css';
 
 function App() {
-
+  
+  const updateTitle = (title) => {
+    const new_title = "Matt Windle | " + title;
+    document.title = new_title;
+  }
   const [Background, SetBackground] = useState("https://images.unsplash.com/photo-1499951360447-b19be8fe80f5")
 
   return (
@@ -20,19 +24,19 @@ function App() {
         <h6>menu</h6>
         <nav>
           <ul>
-            <li><a href="" className="active"><span>Home</span></a></li>
-            <li><a href=""><span>About</span></a></li>
-            <li><a href="" ><span>Projects</span></a></li>
-            <li><a href=""><span>Contact</span></a></li>
+            <li><a href="#" className="active" onClick={() => { updateTitle("Home")}} ><span>Home</span></a></li>
+            <li><a href="#" onClick={() => { updateTitle("About") }}><span>About</span></a></li>
+            <li><a href="#" onClick={() => { updateTitle("Projects") }}><span>Projects</span></a></li>
+            <li><a href="#" onClick={() => { updateTitle("Contact") }}><span>Contact</span></a></li>
           </ul>
         </nav>
         <h6>projects</h6>
         <nav>
           <ul>
-            <li><a href=""><span>React blog</span></a></li>
-            <li><a href=""><span>React Covid app</span></a></li>
-            <li><a href="" ><span>React blog</span></a></li>
-            <li><a href=""><span>React blog</span></a></li>
+            <li><a href="#"><span>React blog</span></a></li>
+            <li><a href="#"><span>React Covid app</span></a></li>
+            <li><a href="#" ><span>React blog</span></a></li>
+            <li><a href="#"><span>React blog</span></a></li>
           </ul>
         </nav>
     </aside>
@@ -43,8 +47,8 @@ function App() {
               <h2>React blog</h2>
               <p>A simple blog made from React for the front end and Node.js to handle the backend requests.</p>
               <div className="mrp-btns">
-                <a href="" className="btn blue">Live project</a>
-              <a href="" className="btn border-white">Views docs</a>
+                <a href="#" className="btn blue">Live project</a>
+              <a href="#" className="btn border-white">Views docs</a>
               </div>
           </div>
         </div>
